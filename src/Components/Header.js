1047a8b001fcc70462852c7651164ade33d5469c
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-06-28 19:07:30 +0200

    update header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../Assets/Images/logo.jpg';
+import NavBar from './NavBar';
+import NavButton from './NavButton';
 
-const Header = () => (
-  <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <h1 className="App-title">
-        Sebastian Gerstel Sollerman
-      <span>
-        Full Stack Web Developer
-      </span>
-    </h1>
-  </header>
-);
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navbarIsActive: false,
+    };
+    this.toggleNavigation = this.toggleNavigation.bind(this);
+  }
+
+  toggleNavigation() {
+    this.setState(prevState => ({ navbarIsActive: !prevState.navbarIsActive }));
+  }
+
+  render() {
+    const { navbarIsActive } = this.state;
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">
+       Sebastian Gerstel Sollerman
+          <span>
+       Full Stack Web Developer
+          </span>
+        </h1>
+        { !navbarIsActive
+          && <NavButton toggleNavigation={this.toggleNavigation} />
+        }
+        { navbarIsActive
+          && <NavBar toggleNavigation={this.toggleNavigation} />
+        }
+
+      </header>
+    );
+  }
+}
+
 
 export default Header;
